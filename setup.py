3ee28987101f025b7750a214c7332a2c560a1e30
Tim Henderson <tim.tadh@gmail.com>
2012-05-12 15:05:46 -0400

    added a setup.py.
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='betterast',
+      version='git master',
+      description=(
+        'An order labeled tree.'
+      ),
+      author='Tim Henderson',
+      author_email='tim.tadh@gmail.com',
+      url='https://www.github.com/timtadh/betterast',
+      license='GPL',
+      packages=['betterast'],
+)
